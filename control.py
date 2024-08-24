0e2dc3d3d772d0da75343038538866ffb1b5c0af
LandryBulls <bulls409@gmail.com>
2024-08-24 11:16:34 -0400

    changed light_controller to control, updating interface.py to use curses module :)

--- control.py
+++ control.py
@@ -14,6 +14,8 @@ from PyDMXControl.controllers import OpenDMXController
 from PyDMXControl.profiles.Generic import Dimmer, Custom
 import threading
 import queue
+import os
+import json
 
 def load_json(json_file):
     with open(json_file, 'r') as f:
@@ -34,14 +36,40 @@ def fft_to_dmx(fft_data, scene_mapping):
     # Return DMX values as a dict of light identities and values
     pass
 
+class SceneManager:
+    def __init__(self, scenes_directory, profiles_directory):
+        self.scenes = self.load_json_files(scenes_directory)
+        self.profiles = self.load_json_files(profiles_directory)
+        self.current_scene = None
+        self.current_profile = None
+
+    def load_json_files(self, directory):
+        data = {}
+        for filename in os.listdir(directory):
+            if filename.endswith('.json'):
+                with open(os.path.join(directory, filename), 'r') as file:
+                    data[filename[:-5]] = json.load(file)
+        return data
+
+    def set_scene(self, scene_name):
+        if scene_name in self.scenes:
+            self.current_scene = self.scenes[scene_name]
+        else:
+            raise ValueError(f"Scene '{scene_name}' not found")
+
+    def set_profile(self, profile_name):
+        if profile_name in self.profiles:
+            self.current_profile = self.profiles[profile_name]
+        else:
+            raise ValueError(f"Profile '{profile_name}' not found")
+
 class LightController(threading.Thread):
-    def __init__(self, audio_listener, dmx_controller, initial_scene, profile):
+    def __init__(self, audio_listener, dmx_controller, scene_manager):
         threading.Thread.__init__(self)
         self.audio_listener = audio_listener
         self.dmx_controller = dmx_controller
-        self.current_scene = initial_scene
+        self.scene_manager = scene_manager
         self.running = threading.Event()
-        self.scene_queue = queue.Queue()
 
     def run(self):
         self.running.set()
@@ -49,14 +77,22 @@ class LightController(threading.Thread):
             fft_data = self.audio_listener.get_fft_data(timeout=0.1)
             if fft_data is not None:
                 dmx_values = self.process_fft(fft_data)
-                self.dmx_controller.set_channels(dmx_values)
+                self.dmx_controller.set_channels(dmx_values) # will need to interrogate this further
             
             self.check_scene_updates()
 
     def process_fft(self, fft_data):
-        # Apply current scene's mapping to fft_data
-        # Return DMX values as a dict of light identities and values
-        pass
+        dmx_values = {}
+        scene = self.scene_manager.current_scene
+        profile = self.scene_manager.current_profile
+
+        for light in scene['lights']:
+            if light['name'] in profile['lights']:
+                profile_light = next(l for l in profile['lights'] if l['name'] == light['name'])
+                light_dmx = self.calculate_light_dmx(light, profile_light, fft_data)
+                dmx_values.update(light_dmx)
+
+        return dmx_values
 
     def change_scene(self, new_scene):
         self.scene_queue.put(new_scene)
